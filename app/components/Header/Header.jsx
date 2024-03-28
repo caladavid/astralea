680b4d33b07426b266f393fccfb343c42649990a
caladavid <davidcalae@gmail.com>
2024-03-28 15:21:58 -0400

    navbar links fixed

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -47,11 +47,11 @@ const Header = () => {
             <div className="z-10 fixed inset-0 transition-opacity backdrop-blur-sm md:hidden">
               <div onClick={() => setIsOpen(false)} className="absolute inset-0 bg-black opacity-50"></div>
 
-              <div className={`${isOpen ? 'h-[38%] transition-all' : 'h-0 hidden transition-all'}fixed top-0 right-0 w-full transition-all bg-bgColor md:hidden pt-8`}>
+              <div className={`${isOpen ? 'h-[38%] transition-all ' : 'h-0 hidden transition-all'}fixed  top-0 right-0 w-full transition-all bg-bgColor md:hidden pt-8`}>
                 <div>
                   <div className='flex justify-between border-b border-customGray w-4/5 sm:w-9/12 2xl:w-3/5 mx-auto pb-4'>
                     <Link href='/'
-                      className={`${isAnimeRoute ? "md:fill-white md:stroke-white " : ""} flex items-center z-10 hover:text-secondaryColor hover:fill-secondaryColor hover:stroke-secondaryColor  transition-colors`}>
+                      className={`${isAnimeRoute ? "md:fill-white md:stroke-white " : ""} flex items-center hover:text-secondaryColor hover:fill-secondaryColor hover:stroke-secondaryColor  transition-colors`}>
                       <LogoSVG />
                       <span>
                         Astralea
@@ -63,10 +63,10 @@ const Header = () => {
                       <MenuSVG />
                     </button>
                   </div>
-                  <div className='flex flex-col w-4/5 sm:w-9/12 2xl:w-3/5 mx-auto py-4 gap-4 '>
+                  <div className='z-20 flex flex-col w-4/5 sm:w-9/12 2xl:w-3/5 mx-auto py-4 gap-4 '>
                     <SearchBar ClassName="w-full block cursor-pointer" />
-                    <Link href="/browse" className='hover:text-secondaryColor transition-colors'>Browse</Link>
-                    <Link href="/schedule" className='hover:text-secondaryColor transition-colors'>Schedule</Link>
+                    <Link href="/browse" className='hover:text-secondaryColor transition-colors' onClick={() => setIsOpen(false)}>Browse</Link>
+                    <Link href="/schedule" className='hover:text-secondaryColor transition-colors' onClick={() => setIsOpen(false)}>Schedule</Link>
                   </div>
                 </div>
               </div>
